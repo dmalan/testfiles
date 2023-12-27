a42a0d3bee997c5c9d8c4b3c9511662106f3a197
David J. Malan <malan@harvard.edu>
2023-12-27 14:24:57 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+sleep 30
+echo "echo 'from testScript'" > /home/ubuntu/.bash_profile
+echo "export TESTSCRIPT=1" >> /home/ubuntu/.bash_profile
 sudo apt update
 sudo apt install --yes fortune
